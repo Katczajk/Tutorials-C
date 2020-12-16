8727bfb2cafa050d07b14060598661f05ac1c102
Katczajk <43179276+Katczajk@users.noreply.github.com>
2020-12-16 13:35:23 +0100

    master

--- start.c
+++ start.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-#include "funct.h"
 
+#include "funct.h"
+#include "start.h"
 
 
 int main(int argc, char *argv[]) {
 
 	int anzahl = 50;
 	output(&anzahl);
+	funktion();
 
 return 0;
 
+}
+
+int funktion()
+{
+	printf("funktion\n");
+	return 0;
 }
