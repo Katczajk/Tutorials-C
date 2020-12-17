f5c57aa2858ee25e34d9983643b3b2f40c3f973b
Katczajk <43179276+Katczajk@users.noreply.github.com>
2020-12-17 21:39:14 +0100

    master

--- start.c
+++ start.c
@@ -8,18 +8,19 @@
 	struct Videos
 	{
 		int laenge;
-		char *autor[13];
+		char autor[50];
 	} video;
 
 int main(int argc, char *argv[]) {
 
 	char autor[13] = "John Connery";
 
-	video.laenge = 90;
-	video.*autor = &autor;
-
-	printf("Der Film hat eine Laenge von %i vom Autor %s", video.laenge, video.autor);
-
+	struct Videos Video1;  
+	Video1.laenge = 10;
+	strcpy(Video1.autor, autor);
+	
+	printf("Der Film hat eine Laenge von %imin vom Autor %s \n", Video1.laenge, Video1.autor);
+	
 	const char myString[] = { 'H','i','\0' };
 	const char myString2[] = "Hi";
 	char ergebnis[sizeof(myString2)];
@@ -33,8 +34,14 @@ int main(int argc, char *argv[]) {
 	printf("String mit Array	 	-> %s\n", myString);
 	printf("String mit \"\" geschrieben 	-> %s\n", myString2);
 	printf("String Ergebnis mit Strcpy 	-> %s\n", ergebnis);
+	
+	char input[] = "Test";  /* create some char unknown lengs calc with input */
+	char String[strlen(input)]; /* setup length from input */
+	strcpy(String,input); /* copy string input to String */
+	printf("Ausgabe %s plain String\n",String); /* output content to console */
 
-    system("pause");
+	system("pause");
+
 	return 0;
 
 }
